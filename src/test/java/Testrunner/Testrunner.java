85ca4cd18d002e95e01decdd26eeed0acad2b776
ntt22877 <yugeshmuppalla2003@gmail.com>
2025-09-29 22:02:37 +0530

    commit and push

---
+++ src/test/java/Testrunner/Testrunner.java
@@ -0,0 +1,22 @@
+package Testrunner;
+import io.cucumber.junit.Cucumber;
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Features/login.feature",
+        glue = {"StepDefinitions"},
+        monochrome = true,
+        plugin = {"pretty",
+                "json:target/cucumber-reports/Cucumber.json",
+                "html:target/cucumber-reports/cucumber.html",
+                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
+)
+
+public class Testrunner  {
+    // No @Test method required
+}
